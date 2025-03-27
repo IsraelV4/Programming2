07b92c7f5e3044e7c167e11790c25c67d68534ab
IsraelV4 <isisraelvel2018@gmail.com>
2025-03-27 17:25:12 -0400

    first commit

---
+++ Day1/Account.java
@@ -0,0 +1,8 @@
+public class Account {
+    public double Capital, Rate;
+    public int Term;
+
+    public double Compute_Balance() {
+        return Math.pow(1+Rate, Term) * Capital;
+    }
+}
---
+++ Day1/day1.java
@@ -0,0 +1,24 @@
+//     public static void main(String[] args) {
+//         Account Josh = new Account();
+//         Account Bob = new Account();
+//         Josh.Capital = 200.50; Josh.Rate = .02; Josh.Term =5;
+//         Bob.Capital = 1000.07; Bob.Rate = .04; Bob.Term =10;
+//         System.out.println("Josh Account balance will be: " + Josh.Compute_Balance());
+//         System.out.println("Bob Account balance will be: " + Bob.Compute_Balance());
+//     }
+//     // public static double FindAvg(int[] numbers) {
+//     //     double tmp = 0;
+//     //     for (int i=0; i<numbers.length;i++) {
+//     //         tmp+=numbers[i];
+//     //     }
+//     //     return tmp/numbers.length;
+//     // }
+
+
+// }
+
+public class day1 {
+    public static void main(String[] args) {
+        
+    }
+}
