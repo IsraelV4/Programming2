06bfc7bf82e8af70f3be8a16c2154c47d2276a61
IsraelV4 <isisraelvel2018@gmail.com>
2025-04-01 15:04:47 -0400

    finished the file for entering information, still needs way to detect duplicate information, but first I should make a different jframe to store all of this information inside of

--- Project/Political.java
+++ Project/Political.java
@@ -10,7 +10,7 @@ import javax.swing.*;
 public class Political extends JFrame implements ActionListener {
 
     /*
-1. Request some information from voter --> 
+1. Request some information from voter --> check
 2. Request what party a voter wish to vote for in presidential election
 3. Request for the party in Senate, House and Governorship elections
 4. Disallow double voting
@@ -22,36 +22,53 @@ public class Political extends JFrame implements ActionListener {
     JTextField[] info = new JTextField[11];
 
     JButton button = new JButton("Send");
-    String[] data = new String[] {"First", "Middle", "Last", "SSN", "number", "address line 1", "address line 2", "street"," zip code", "city", "state"};
+    String[] data = new String[] {"First name", "Middle name (Optional)", "Last name", "SSN", "phone number", "address line 1", "address line 2 (Optional)", "street address"," zip code", "city", "state"};
+    JLabel error = new JLabel("Hello");
 
     public Political(String name) {
         super(name);
         setLocation(0,0);
         setSize(500,500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
+        
 
         setLayout(new GridLayout(info.length+2, 3, 5, 5));
-        add(new JLabel("Please enter your information here"));
-        add(new JLabel()); add(new JLabel());
+        add(new JLabel("Please enter"));
+        add(new JLabel("your information")); add(new JLabel("                  here"));
         for (int i=0; i<info.length; i++) {
             info[i] = new JTextField();
             add(new JLabel( " "+data[i]));
             add(new JLabel());
             add(info[i]);
         }
-        add(button, BorderLayout.SOUTH);
+        button.addActionListener(this);
+        add(button);
+        add(error);
+        setVisible(true);
     }
 
     
     public static void main(String[] args) {
         Political frame = new Political("Political Project");
-        frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        if (e.getSource()==button) {
+            String[] tmp = new String[info.length];
+            for (int i=0; i<tmp.length; i++) {
+                tmp[i] = info[i].getText();
+                if (i!=1 && i!=6 && tmp[i].equals("")) {
+                    error.setText("<html>"+"Invalid information, please fix your [" + data[i] + "]" + "</html>");
+                    return;
+                }
+            }
+            error.setText("");
+            setVisible(false);
+        }
+        else {
+            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        }
     }
 }
