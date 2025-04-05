7516129c2dcfbc3d22c7ccb282419aa764bc9500
IsraelV4 <isisraelvel2018@gmail.com>
2025-04-05 13:26:49 -0400

    Prepared for the start of the competition

---
+++ Competitions/TeamCodeSpring2025/Template.java
@@ -0,0 +1,17 @@
+package Competitions.TeamCodeSpring2025;
+
+import java.util.*;
+import java.io.*;
+
+@SuppressWarnings("unused")
+public class Template {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+        input.close();
+    }
+    
+}
---
+++ Competitions/TeamCodeSpring2025/one.java
@@ -0,0 +1,17 @@
+package Competitions.TeamCodeSpring2025;
+
+import java.util.*;
+import java.io.*;
+
+@SuppressWarnings("unused")
+public class one {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+        input.close();
+    }
+    
+}
